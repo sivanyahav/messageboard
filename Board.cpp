bb46e0ff29170abf23696ca83499327eaeb3c31f
Sivan Yahav <68699211+sivanyahav@users.noreply.github.com>
2021-03-18 12:19:25 +0200

    Add files via upload

---
+++ Board.cpp
@@ -0,0 +1,75 @@
+
+#include <iostream>
+#include "Board.hpp"
+using namespace std;
+using namespace ariel;
+
+const char defaultChar = '_';
+namespace ariel {
+void Board::resizeBoard(int row, int col){
+
+    this-> rows =row;
+    this->cols =col;
+    this-> board.resize(row);
+
+    for(int i=0; i < row; i++){
+        board.at(i).resize(col,defaultChar);
+    }
+
+}
+
+void Board::post(int row, int col, Direction d, string message){
+    int messageSize = message.length();
+    bool flag = (d == Direction::Horizontal);
+    const int resizeLength=10;
+
+    if(flag){
+        resizeBoard(std :: max(this->rows, row+1), std::max(this->cols, col+messageSize+1));
+        
+    }
+    else{
+        resizeBoard(std :: max(this->rows, row+messageSize+1), std::max(this->cols, col+1));
+    }
+
+    for (int i = 0; i < messageSize; i++)
+    {
+       board.at(row).at(col) = message[i];
+       cout << "msg[i]: " << message[i] <<"\n";
+       cout << "board[" << row << "]" << "[" << col << "]: " << board.at(row).at(col) <<"\n";
+       flag? col++ : row++;
+    }
+    
+}
+
+
+string Board::read(int row, int col, Direction d, int num){
+
+    string s;
+    bool flag = (d == Direction::Horizontal);
+
+    flag? resizeBoard(std::max(this->rows,row+1),std::max(this->cols,col+num+1)): resizeBoard(std::max(this->rows,row+num+1),std::max(this->cols,col+1));
+
+
+    for(int i=0; i<num; i++){
+        s+=board.at(row).at(col);
+        cout << "i is " << i << " row is "<< row <<" col is "<< col << " s is: " <<s<<"\n";
+        flag? col++ : row++;
+    }
+        
+    return s;
+}
+
+
+void Board::show(){
+
+    for (int i = 0; i < this->rows; i++) {
+        cout << i << ": ";
+        for (int j = 0; j < this->cols; j++) {
+            cout << this->board[i][j];
+        }
+        cout<< "\n";
+    }
+    cout << "\n" << endl;
+}
+}
+
---
+++ Board.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Direction.hpp"
+#include <string>
+#include <vector>
+using namespace std;
+
+namespace ariel {
+
+    class Board {
+
+        private:
+        int rows, cols;
+        vector<vector<char>> board;
+        void resizeBoard(int row, int col);
+
+        public:
+            Board(){
+                board=vector<vector<char>>(10, vector<char>(10,'_'));
+                rows=10;
+                cols=10;
+             };
+            ~Board(){
+                // delete rows;
+                // delete cols;
+                board.shrink_to_fit();
+                };
+            void post(int row, int col, Direction d, string message);
+            string read(int row, int col,Direction d, int num);
+            void show();
+    };
+}
