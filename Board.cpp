f33dd67d17e52a1480a13b046e93ec1b3fa00b54
Sivan Yahav <68699211+sivanyahav@users.noreply.github.com>
2021-03-24 13:39:02 +0200

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -6,15 +6,103 @@ using namespace ariel;
 
 const char defaultChar = '_';
 namespace ariel {
+
+     unsigned int Board:: updateRows(unsigned int n1, unsigned int n2, bool flag, unsigned int num){
+         unsigned int max=0;
+            //Optionaly for max Row
+            if(n1>n2){
+                if(flag){
+                    if(n2-1<this->minRow){ this->minRow=n2-1;}
+                }
+                else{
+                    if(n2-num-1< this->minRow){ this->minRow=n2-num;}
+                }
+                 
+                if(n1>this->maxRow){this->maxRow=n1;}
+                max=n1;
+            }
+
+            else{
+                if(flag){
+                    if(n1-1<this->minRow){this->minRow=n1-1;}
+                }
+                else{
+                    if(n1-num-1<this->minRow){this->minRow=n1-num;}
+                }
+                
+                if(n2>this->maxRow){this->maxRow=n2;}
+                max=n2;
+            }
+
+        
+        return max;      
+    }
+
+//------------------------------------------------------------------------------------------
+
+    unsigned int Board:: updateCols(unsigned int n1, unsigned int n2, bool flag, unsigned int num){
+        unsigned int max=0;
+
+        if(n1>n2){
+            if(flag){
+                if(n2-num-1<this->minCol){ this->minCol=n2-num;}
+            }
+            else{
+                if(n2-1<this->minCol){this->minCol=n2-1;}
+            }
+            if(n1>this->maxCol){this->maxCol=n1;}
+            max=n1;
+        }
+
+        else{
+            if(flag){
+                if(n1-num-1<this->minCol){this->minCol=n1-num;}
+            }
+            else{ 
+                if(n1-1<this->minCol){ this->minCol=n1-1;}
+            }
+            if(n2>this->maxCol){this->maxCol=n2;}
+            max=n2;
+        }
+        return max;      
+    }
+
+ //------------------------------------------------------------------------------------------
+
+    void Board::update(unsigned int row, unsigned int col, bool flag, unsigned num){
+
+        //first time we update the board
+        if(this->maxRow==0 && this->minRow==INT32_MAX && this->maxCol==0 && this->minCol==INT32_MAX){
+             if (flag){
+                this->maxRow=row+1;
+                this->maxCol=col+num;
+                this->minRow=row;
+                this->minCol=col;
+             }
+             else{
+                this->maxRow=row+num;
+                this->maxCol=col+1;
+                this->minRow=row;
+                this->minCol=col;
+             }
+             resizeBoard(this->maxRow, this->maxCol);
+        }
+
+        else{
+            flag? resizeBoard(updateRows(this->maxRow, row+1, flag, num),updateCols(this->maxCol, col+num+1, flag, num)):
+                  resizeBoard(updateRows(this->maxRow, row+num+1, flag, num),updateCols(this->maxCol, col+1, flag, num));
+        }
+        
+    }
+
+ //------------------------------------------------------------------------------------------
     
     /*
     This function resize board size,
     and updates the number of rows and columns
     */
-    void Board::resizeBoard(int row, int col){
+    void Board::resizeBoard(unsigned int row, unsigned int col){
 
-        this-> rows =row;
-        this-> cols =col;
         this-> board.resize(row);
 
         for(int i=0; i < row; i++){
@@ -23,37 +111,37 @@ namespace ariel {
 
     }
 
+//------------------------------------------------------------------------------------------
+
     /*
     This function writes to the message board 
     and if necessary increases the size of the board.
     */
-    void Board::post(int row, int col, Direction d, string message){
+    void Board::post(unsigned int row, unsigned int col, Direction d, const string &message){
         int messageSize = message.length();
         bool flag = (d == Direction::Horizontal);
 
-        flag? resizeBoard(std :: max(this->rows, row+1), std::max(this->cols, col+messageSize+1)):
-              resizeBoard(std :: max(this->rows, row+messageSize+1), std::max(this->cols, col+1));
+        update(row,col,flag,messageSize);
 
-        for (int i = 0; i < messageSize; i++){
-        board.at(row).at(col) = message[i];
+        for (char ch: message){
+        board.at(row).at(col) = ch;
         flag? col++ : row++;
         }
     
     }
+
+//------------------------------------------------------------------------------------------
 
     /*
     This function reads messages from the message board
     and if necessary increases the size of the board, and
     updates the default characters.
     */
-    string Board::read(int row, int col, Direction d, int num){
-
+    string Board::read(unsigned int row, unsigned int col, Direction d, unsigned int num){
         string s;
         bool flag = (d == Direction::Horizontal);
 
-        flag? resizeBoard(std::max(this->rows,row+1),std::max(this->cols,col+num+1)):
-              resizeBoard(std::max(this->rows,row+num+1),std::max(this->cols,col+1));
-
+        update(row,col,flag,num);
 
         for(int i=0; i<num; i++){
             s+=board.at(row).at(col);
@@ -63,13 +151,14 @@ namespace ariel {
         return s;
     }
 
-
+ //------------------------------------------------------------------------------------------
     void Board::show(){
 
-        for (int i = 0; i < this->rows; i++) {
+        for (unsigned int i = this->minRow; i < this->maxRow; i++) {
             cout << i << ": ";
-            for (int j = 0; j < this->cols; j++) {
-                cout << this->board[i][j];
+           
+            for (unsigned int j = this->minCol; j < this->maxCol; j++) {
+                    cout << this->board[i][j];
             }
             cout<< "\n";
         }
